0d8b2a9b903cb88648ff328955cc9d22810f5d7c
herow <herow2011@sina.com>
2016-06-28 19:16:30 +0800

    db for planning

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "Application.h"
+#include "qtranslator.h"
+int main( int argc, char ** argv )
+{
+    // Create application object. All the initialisation stuff happens in there
+    Application a(argc, argv);
+
+    // Quit application now if user doesn't want to see the UI
+    if(a.dontShowMainWindow())
+        return 0;
+    QTranslator qtTranslator;
+    qtTranslator.load("sqlb_zh.qm");
+    a.installTranslator(&qtTranslator);
+    // Run application
+    return a.exec();
+}
